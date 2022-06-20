a4bfc00becd7b25b00a6558f7750add747675b82
linhthuydanhbac <tuan.dc@urbox.vn>
2022-06-20 20:56:08 +0700

    add README.md 🤔

--- Config.py
+++ Config.py
@@ -1,15 +1,11 @@
 # Redis Connection
-REDIS_CONFIG_STAGING = {
-    "host": "10.0.1.22",
-    "port": "6380",
-    "db": "12"
-}
+
 
 # MySQL Connection
 MYSQL_CONFIG_DEVELOP = {
     "host": "remotemysql.com",
     "port": "3306",
     "user": "nHJKGmsGP4",
-    "password": "OSnbbU7RtN",
+    "password": "password",
     "auth_plugin": "mysql_native_password"
 }
